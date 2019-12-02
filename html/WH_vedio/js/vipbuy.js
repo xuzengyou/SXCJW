e6787d4c91d8fe534173474a063086686bc9d499
997167585@qq.com <997167585@qq.com>
2019-12-02 18:56:50 +0800

    vip

---
+++ html/WH_vedio/js/vipbuy.js
@@ -0,0 +1,32 @@
+$(function(){
+    //搜索功能
+    $(".btn1").click(function(){
+        var Word=$("#q").val();
+        if(Word){
+            window.location.href="http://img.10xunc.com/备用件/html/search.html?keyWord="+Word;
+        }
+        
+    });
+    $("#q").keyup(function(e){
+        if(e.keyCode==13){
+            var Word=$("#q").val();
+            if(Word){
+                window.location.href="http://img.10xunc.com/备用件/html/search.html?keyWord="+Word;
+            }
+        }
+    });
+    //
+    $("div.list").click(function(){
+        $(this).addClass("ac").siblings().removeClass("ac");
+        $("div.list .dui").removeClass("ac");
+        $("div.ac .dui").addClass("ac");
+        
+    })
+    
+
+
+
+
+
+
+})
