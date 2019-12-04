2705a8dfef49da70f4fe0357764816747620f205
997167585@qq.com <997167585@qq.com>
2019-12-04 18:13:10 +0800

    个人中心

--- html/person/js/Vipperson.js
+++ html/person/js/Vipperson.js
@@ -1,5 +1,5 @@
 $(function(){
-    $("div.Lth span").click(function(){
+    $("div.Lth div").click(function(){
         $(this).addClass("ac").siblings().removeClass("ac");
         var id=$(this).attr("data-id");
         $(id).addClass("ac").siblings().removeClass("ac");
